90c1fda249c9b485abc42bc838346d656e00cfc5
jason-lim <jason_lim@rp.edu.sg>
2022-08-30 19:48:54 +0800

    Init commit

---
+++ com.sddevops.pullfetch_maven/src/main/java/com/sddevops/com/sddevops/pullfetch_maven/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.com.sddevops.pullfetch_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
